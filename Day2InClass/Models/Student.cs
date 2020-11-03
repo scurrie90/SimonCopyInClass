d1100dcc38d1d9c9b640068b89f3ed36497f5470
philweier <pweier@bcit.ca>
2020-11-03 10:54:30 -0800

    refactor Program to move Student to it's own code file

---
+++ Day2InClass/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2InClass.Models
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string StudentNumber { get; set; }
+
+        public Student(string StudentNumber, string Name)
+        {
+            this.StudentNumber = StudentNumber;
+            this.Name = Name;
+        }
+    }
+}
--- Day2InClass/Program.cs
+++ Day2InClass/Program.cs
@@ -1,36 +1,25 @@
-﻿using System;
+﻿using Day2InClass.Models;
+using System;
 
 namespace Day2InClass
 {
-    class Student
-    {
-        public string Name { get; set; }
-        public string StudentNumber { get; set; }
-
-        public Student(string StudentNumber, string Name)
-        {
-            this.StudentNumber = StudentNumber;
-            this.Name = Name;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Provide a student number.");
             string id = Console.ReadLine();
-            
+
             Console.WriteLine("Provide a student name.");
             string name = Console.ReadLine();
-            
-            // create an instance of the Student Class
-             Student stud1 = new Student(id, name);
 
-            Console.WriteLine("New Student: Number = " + stud1.StudentNumber + " Name = " + stud1.Name);           
+            // create an instance of the Student Class
+            Student stud1 = new Student(id, name);
+            Console.WriteLine("New Student: Number = " + stud1.StudentNumber + " Name = " + stud1.Name);
             Console.WriteLine("Are the above values correct? Y/N ");
             string correct = Console.ReadLine();
-            
-            if(correct != "Y")
+
+            if (correct != "Y")
             {
                 Console.WriteLine("Provide a student number.");
                 id = Console.ReadLine();
@@ -40,7 +29,6 @@ namespace Day2InClass
 
                 stud1.StudentNumber = id;
                 stud1.Name = name;
-
                 Console.WriteLine("New Student: Number = " + stud1.StudentNumber + " Name = " + stud1.Name);
             }
             Console.WriteLine("Thanks, student has been created");
