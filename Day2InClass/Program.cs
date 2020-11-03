237334c89dde1d834ec595c7bbba7aa9897fd450
philweier <pweier@bcit.ca>
2020-11-03 10:00:42 -0800

    add user input to instanciate a Student object

--- Day2InClass/Program.cs
+++ Day2InClass/Program.cs
@@ -6,14 +6,27 @@ namespace Day2InClass
     {
         public string Name { get; private set; }
         public string StudentNumber { get; private set; }
+
+        public Student(string StudentNumber, string Name)
+        {
+            this.StudentNumber = StudentNumber;
+            this.Name = Name;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Provide a student number.");
+            string id = Console.ReadLine();
+            
+            Console.WriteLine("Provide a student name.");
+            string name = Console.ReadLine();
+            
             // create an instance of the Student Class
-            Student stud1 = new Student(); 
-            Console.WriteLine(stud1.StudentNumber + " " + stud1.Name);
+             Student stud1 = new Student(id, name);
+
+            Console.WriteLine("Student 1 is: " + stud1.StudentNumber + " " + stud1.Name);
             Console.Read();
         }
     }
