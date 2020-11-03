45fbf50758adef1fc040ab7201777ea017149b43
philweier <pweier@bcit.ca>
2020-11-03 09:44:15 -0800

    add Student class and create an instance

--- Day2InClass/Program.cs
+++ Day2InClass/Program.cs
@@ -2,11 +2,19 @@
 
 namespace Day2InClass
 {
+    class Student
+    {
+        public string Name { get; private set; }
+        public string StudentNumber { get; private set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create an instance of the Student Class
+            Student stud1 = new Student(); 
+            Console.WriteLine(stud1.StudentNumber + " " + stud1.Name);
+            Console.Read();
         }
     }
 }
