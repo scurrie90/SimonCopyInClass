5b91f431999238eb760ea9afd008759ee1e8fa34
philweier <pweier@bcit.ca>
2020-11-03 09:31:53 -0800

    Add project files.

---
+++ Day2InClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day2InClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
